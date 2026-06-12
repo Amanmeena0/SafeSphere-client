a7cba1ec2cb1daa02f888e6457291b23edfd2506
Amanmeena0 <meenaaman581@gmail.com>
2026-06-12 11:07:56 +0530

    refactor: remove TypeScript ignore comment for global CSS import

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { ClerkProvider } from '@clerk/clerk-react'
 import App from './App.tsx'
-// Suppress TS error for side-effect CSS import when no global d.ts is present
-// @ts-ignore
 import '@/global.css'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
